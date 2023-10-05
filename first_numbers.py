6b1b2f5dad902f4afe75de6cf5daa4c8e1275b86
Sonofneli7 <94796497+Sonofneli7@users.noreply.github.com>
2023-10-05 19:52:50 -0400

    Add files via upload

---
+++ first_numbers.py
@@ -0,0 +1,7 @@
+# using the range() function, making numerical lists, does count 1-4
+for value in range(1,5):
+    print(value)
+
+# using range() to make a list of numbers
+numbers = list (range(1,6))
+print(numbers)
